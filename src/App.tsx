70771540abc12705db8b8a3372e3824b075f7cdb
Punit <Punitmistr@gmail.com>
2024-05-06 23:03:19 +0530

    added changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,29 @@ import gsap from "gsap";
 export default function App() {
   const container = useRef();
 
-  useGSAP(() => {
-    gsap.fromTo(".box",
-      {x:40,backgroundColor:"green",width:'50px'},{y:200,x:150,backgroundColor:"blue",width:'150px'}
+  const animate = () => {
+    gsap.fromTo(
+      ".box",
+      {x:0,width:100,background:'blue'},
+      {x:200,rotation:360,
+        scale:5,
+        marginTop:50,
+        display: 'flex',
+        justifyContent: 'center',
+        
+        duration:2
+      }
     );
-  });
+  };
+
+  useGSAP(() => {}, []); // Empty useGSAP hook
 
   return (
-    <div ref={container} className="box ">
-      Hello
-    </div>
+    <>
+      <div ref={container} className="box w-50">
+        Hello
+      </div>
+      <button onClick={animate}>Animate</button>
+    </>
   );
 }
